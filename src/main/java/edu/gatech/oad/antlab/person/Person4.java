b6d41e72c4d00a954a79b5f29b9cf5944336d9b3
Alex.Booth <alexbooth08@gmail.com>
2018-01-28 14:43:52 -0500

    removed useless file

--- src/main/java/edu/gatech/oad/antlab/person/Person4.java
+++ src/main/java/edu/gatech/oad/antlab/person/Person4.java
@@ -33,7 +33,7 @@ public class Person4 {
       //Person 4 put your implementation here
       String result = "";
       for (int i = 0; i < input.size(); i++) {
-
+        result += input.charAt(i);
       }
       return result;
     }
