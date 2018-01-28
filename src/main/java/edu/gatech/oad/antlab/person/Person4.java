ca7dcc6759946101dda96eb71958a486f8e36d07
Alex.Booth <alexbooth08@gmail.com>
2018-01-28 14:24:04 -0500

    edited calc method

--- src/main/java/edu/gatech/oad/antlab/person/Person4.java
+++ src/main/java/edu/gatech/oad/antlab/person/Person4.java
@@ -30,8 +30,8 @@ public class Person4 {
      * @return the modified string
      */
     private String calc(String input) {
-      //Person 4 put your implementation here
-      return null;
+      String result = input;
+      return result;
     }
 
     /**
